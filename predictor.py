94a34f38b340ac2f4c345bd9c2b314cf39e99505
B Indrajith Shetty <indrajitshetty@gmail.com>
2018-02-12 00:53:44 -0500

    got 65%

--- predictor.py
+++ predictor.py
@@ -2,7 +2,7 @@
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator,load_img,img_to_array
 
 
 def get_model():
@@ -34,9 +34,16 @@ model=get_model()
 model.load_weights('first_try.h5')
 
 test_datagen = ImageDataGenerator(rescale=1./255)
-test_generator=test_datagen.flow_from_directory('../data/test',
+test_generator=test_datagen.flow_from_directory(
+        '../data/test',
         target_size=(150, 150),  
-        batch_size=600,
+        batch_size=16,
         class_mode='categorical')
-score=model.evaluate_generator(test_generator,600//16,use_multiprocessing=True)
+
+#img =load_img("/Users/Indra/Documents/Online courses/Udacity/paid courses/machine-learning-engineer/GitHub-Repo/data/test/nevus/ISIC_0012551.jpg", target_size=(150, 150))
+#x = img_to_array(img)
+#x=x.reshape((1,150,150,3))
+#pred=model.predict(x)
+#print(pred)
+score=model.evaluate_generator(test_generator)
 print(score[1])
